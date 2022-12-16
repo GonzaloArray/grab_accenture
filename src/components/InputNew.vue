1cbb924c169d472abec2b6eb3cdc53f092a974f6
GonzaloArray <gonzalo.arrayaran@gmail.com>
2022-12-16 20:31:02 -0300

    Add: project GRAB

---
+++ src/components/InputNew.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, ref, reactive, defineProps } from "vue";
+//const props = defineProps(["onNewItem"]);
+const text = ref("");
+const emits = defineEmits(["onNewItem"]);
+
+function handleKeyDown(evt) {
+  if (evt.key === "Enter") {
+    emits("onNewItem", text);
+    text.value = "";
+  }
+}
+</script>
+
+<template>
+  
+  <input class="form-control mb-4" v-model="text" @keydown="handleKeyDown" placeholder="Add to card" />{{ text }}
+
+</template>
+
+<style scoped>
+
+</style>
