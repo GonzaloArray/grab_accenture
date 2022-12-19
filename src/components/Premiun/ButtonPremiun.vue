0c3c55a4ce30d70987c4d79b33724d60be4cb949
GonzaloArray <gonzalo.arrayaran@gmail.com>
2022-12-19 12:43:24 -0300

    Add: project GRAB

---
+++ src/components/Premiun/ButtonPremiun.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+    href: String,
+    icon: String,
+    text: String
+})
+
+</script>
+
+<template>
+    <RouterLink class="btn__premiun rounded-2 text-light" :to="'/' + href">
+        <div class="d-flex align-items-center justify-content-center">
+            <span class="material-icons-outlined me-2">
+                {{icon}}
+            </span>
+            <p class="m-0">{{text}}</p>
+        </div>
+    </RouterLink>
+</template>
+
+<style scoped>
+.btn__premiun {
+    padding: .4rem 40px;
+    text-decoration: none;
+    box-shadow: inset 18px 64px 231px -119px rgb(209, 4, 4);
+    background: linear-gradient(to right, rgba(255, 183, 3, 1) 0%, rgba(255, 183, 3, 1) 8%, rgba(251, 133, 0, 1) 38%, rgba(251, 133, 0, 1) 90%, rgba(251, 133, 0, 1) 100%);
+}
+
+.btn__premiun:hover {
+    background: #f69423;
+}
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,14 +1,59 @@
 <script setup>
 import Todo from '../components/Todo.vue';
+import ButtonPremiun from '../components/Premiun/ButtonPremiun.vue'
 
+
+import confetti from "canvas-confetti";
+import BrackgroundEffect from '../components/Background/BrackgroundEffect.vue';
+import { ref } from '@vue/reactivity';
+import { useBackground } from '../store/useBackground';
+
+
+const backColor = useBackground();
+
+
+function handleConfetti() {
+    var end = Date.now() + (2 * 1000);
+
+    // go Buckeyes!
+    var colors = ['#71A5D3', '#ffffff'];
+
+    (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
+}
 
 
 </script>
 
 <template>
-    <Todo />
+    <section :style="'background-color:'+ backColor?.back?.color ">
+        <Todo />
+        <div class="d-flex m-2">
+            <ButtonPremiun @click="handleConfetti" href="" icon="workspace_premium" text="Try Premium for free"/>
+    
+        </div>
+        <BrackgroundEffect />
+    </section>
 </template>
 
-<style>
+<style scoped>
 
 </style>
