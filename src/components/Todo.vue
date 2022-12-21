bc0691e3b3e8134fdfe89be26c778b0dfbddb745
GonzaloArray <gonzalo.arrayaran@gmail.com>
2022-12-21 02:45:33 -0300

    Style: background dashboard

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,6 +3,7 @@ import { onMounted, ref, reactive } from "vue";
 import InputNew from "./InputNew.vue";
 
 const count = ref(0);
+
 let boards = reactive([
     {
         id: crypto.randomUUID(),
@@ -20,23 +21,41 @@ let boards = reactive([
         items: [{ id: crypto.randomUUID(), title: "Hola a todos 2" }],
     },
 ]);
-function startDrag(evt, boardId, itemId) {
-    console.log(boardId, itemId);
-    evt.dataTransfer.dropEffect = "move";
-    evt.dataTransfer.effectAllowed = "move";
-    evt.dataTransfer.setData("item", JSON.stringify({ boardId, itemId }));
+
+const handleDragStart = (event, boardId, itemId) => {
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('item', JSON.stringify({ boardId, itemId }));
+    event.target.style.backgroundColor = '#023047';
+    event.target.style.color = '#fff';
+    event.target.style.opacity = '.01';
+    event.className = "grabbing";
 }
+
+const handleDragEnd = (event, boardId, itemId) => {
+    event.target.style.opacity = '1';
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = '#000';
+
+}
+
+const handleDragEnter = (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+
+}
+
 function onDrop(evt, dest) {
     const { boardId, itemId } = JSON.parse(evt.dataTransfer.getData("item"));
-    console.log({ boardId, itemId });
+
     const board = boards.find((board) => board.id === boardId);
     const item = board.items.find((item) => item.id === itemId);
     board.items = board.items.filter((i) => i.id !== item.id);
     dest.items.push({ ...item });
 }
 
+
 function handleNewItem(text, board) {
-    console.log(text.value);
+
     board.items.push({ id: crypto.randomUUID(), title: text.value });
 }
 
@@ -63,15 +82,18 @@ function createNewBoard() {
         </nav>
 
         <section class="lists-container">
-            <div class="list" @drop="onDrop($event, board)" @dragover.prevent
-                @dragenter.prevent v-for="board in boards" :key="board.id">
+            <div class="list" @drop="onDrop($event, board)" @dragover.prevent @dragenter.prevent v-for="board in boards"
+                :key="board.id">
                 <h2 class="list-title">{{ board.name }}</h2>
                 <div class="input mx-2">
                     <InputNew @on-new-item="(text) => handleNewItem(text, board)" />
                 </div>
                 <ul class="list-items">
                     <li class="rounded-2 p-2 mt-2 item drag-el" draggable="true"
-                        @dragstart="startDrag($event, board.id, item.id)" v-for="item in board.items" :key="item.id">
+                        @dragstart="handleDragStart($event, board.id, item.id);"
+                        @dragend="handleDragEnd($event, board.id, item.id);"
+                        @dragenter="handleDragEnter($event, board.id, item.id)"
+                        v-for="item in board.items" :key="item.id">
                         {{ item.title }}
                     </li>
 
@@ -86,12 +108,23 @@ function createNewBoard() {
     background-color: #fff;
     margin-bottom: 10px;
     padding: 5px;
+
+}
+
+.dragging {
+    background-color: red !important;
+    opacity: 1;
+    cursor: pointer;
 }
 
 /* Lists */
 
 .lists-container::-webkit-scrollbar {
     height: 2.4rem;
+}
+
+.over {
+    border: 3px dotted #666;
 }
 
 .lists-container::-webkit-scrollbar-thumb {
@@ -116,6 +149,7 @@ function createNewBoard() {
     max-height: calc(100vh - 11.8rem);
     border-radius: 0.3rem;
     margin-right: 1rem;
+    cursor: pointer;
 }
 
 .list:last-of-type {
@@ -158,7 +192,6 @@ function createNewBoard() {
     border-radius: 0.3rem;
     margin-bottom: 0.6rem;
     word-wrap: break-word;
-    cursor: pointer;
 }
 
 .list-items li:last-of-type {
@@ -168,6 +201,4 @@ function createNewBoard() {
 .list-items li:hover {
     background-color: #eee;
 }
-
-
 </style>
