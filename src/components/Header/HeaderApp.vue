2a910f9e9eb778af69580bf16768b0cd4c4160dd
Ivan Gutierrez <ivangonzalogutierrez97@hotmail.com>
2022-12-22 21:04:35 -0300

    landing 22/12

--- src/components/Header/HeaderApp.vue
+++ src/components/Header/HeaderApp.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router';
 import { useUserStore } from "../../store/user.js";
 import { getAuth, signOut } from '@firebase/auth';
 
-const user=useUserStore()
+const user = useUserStore()
 console.log(user.existeUsuario);
 
 function signout() {
@@ -18,7 +18,48 @@ function signout() {
 
 </script>
 <template>
-    <nav class="navbar d-flex justify-content-between shadow">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- <nav class="navbar d-flex justify-content-between shadow">
         <div class="d-flex align-items-center gap-3">
             <RouterLink to="/landing" class="me-3">
                 <img src="../../assets/Images/letraGrab.png" alt="Letra de GRAB">
@@ -44,21 +85,23 @@ function signout() {
                 <span>PREMIUM</span>
             </button>
         </div>
-    </nav>
-
+    </nav> -->
 </template>
 <style scoped>
 .navbar {
     width: 100%;
     background-color: #023047;
-    padding: 0 0;
-    max-height: 3rem;
+        /* padding: 0 0; */
+    /* max-height: 3rem; */
     display: flex;
     justify-content: flex-start;
     align-items: center;
     min-height: 10vh;
+    position: sticky;
+    top: 0;
 }
-.contenedor-botones{
+
+.contenedor-botones {
     width: 22rem;
     height: 10vh;
 }
@@ -84,14 +127,17 @@ function signout() {
     width: 12rem;
     min-height: 100%;
 }
-.btn-premium:hover{
-    box-shadow: 0px 0px 5px 0px rgba(235,225,27,0.67)
+
+.btn-premium:hover {
+    box-shadow: 0px 0px 5px 0px rgba(235, 225, 27, 0.67)
 }
-.btn-premium>img{
+
+.btn-premium>img {
     height: 2rem !important;
     padding-left: 0px !important;
     filter: drop-shadow(1px 1px 1px #000);
 }
+
 .offcanvas {
     max-width: 15rem;
 }
